9e84c7ef702676b9a72964ad8a0a24b8197f2597
Katie Unger <katie.e.unger@gmail.com>
2019-01-08 23:37:05 -0500

    converting HTML to JSX; converting inline styling to JSX format

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import ReactDOM from 'react-dom';
 
 // Create a React component
 const App = () => {
-    return <div>Hi there!</div>;
+    return (
+        <div>
+            <label class="label" for="name"></label>
+            <input id="name" type="text" />
+            <button style= {{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        </div>
+    );
 };
 // Take the React component and show it on the screen
 ReactDOM.render(
