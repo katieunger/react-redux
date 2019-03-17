e0a5d72a8bba0de1e8b8693031ac66242795de26
Katie Unger <katie.e.unger@gmail.com>
2019-03-17 18:58:25 -0400

    viewing request results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import axios from 'axios';
 
 import SearchBar from './SearchBar';
+import keys from '../keys';
 
 class App extends React.Component {
     onSearchSubmit(term) {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID ' + keys.axiosKey
+            }
+        });
     }
 
     render() {
