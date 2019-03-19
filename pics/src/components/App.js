0df2b6c846303289a368177fde9f40263ed9403d
Katie Unger <katie.e.unger@gmail.com>
2019-03-18 23:46:44 -0400

    setting state after async requests - scope issues

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,6 +5,10 @@ import SearchBar from './SearchBar';
 import keys from '../keys';
 
 class App extends React.Component {
+    state = {
+        images: []
+    }
+
     async onSearchSubmit(term) {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
@@ -12,13 +16,16 @@ class App extends React.Component {
                 Authorization: 'Client-ID ' + keys.unsplashKey
             }
         });
-        console.log(response.data.results);
+
+        console.log(this);
+        this.setState({images: response.data.results});
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop:'10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
