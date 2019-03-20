3476a75c1af4daf6d502a3a3f22bc24691973c88
Katie Unger <katie.e.unger@gmail.com>
2019-03-19 23:05:10 -0400

    creating custom clients

---
+++ pics/src/api/unsplash.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+import keys from '../keys';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com',
+    headers: {
+        Authorization: 'Client-ID ' + keys.unsplashKey
+    }
+});
--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import axios from 'axios';
-
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
-import keys from '../keys';
+
 
 class App extends React.Component {
     state = {
@@ -10,11 +9,8 @@ class App extends React.Component {
     }
 
     onSearchSubmit = async (term) => {
-        const response = await axios.get('https://api.unsplash.com/search/photos', {
+        const response = await unsplash.get('/search/photos', {
             params: { query: term },
-            headers: {
-                Authorization: 'Client-ID ' + keys.unsplashKey
-            }
         });
 
         this.setState({images: response.data.results});
