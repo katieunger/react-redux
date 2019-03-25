73ec8cb8689b4cc3723bd2b102e1a5e35653d6d9
Katie Unger <katie.e.unger@gmail.com>
2019-03-24 22:31:55 -0400

    callbacks on image load

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -8,7 +8,10 @@ class ImageCard extends React.Component {
     }
 
     componentDidMount() {
-        console.log(this.imageRef.current);
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+
+    setSpans = () => {
         console.log(this.imageRef.current.clientHeight);
     }
 
