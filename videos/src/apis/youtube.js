3c0eb9feb38794e6662b0219d66d1e7fa482b7f4
Katie Unger <katie.e.unger@gmail.com>
2019-03-27 22:27:04 -0400

    accessing the youtube api and searching for videos

---
+++ videos/src/apis/youtube.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+import keys from '../keys';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: 'snippet',
+    maxResults: 5,
+    key: keys.youtubeKey
+});
