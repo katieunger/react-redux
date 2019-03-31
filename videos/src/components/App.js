18d6a1871131f2d39ab1db22146ff242a06ac6f2
Katie Unger <katie.e.unger@gmail.com>
2019-03-31 18:27:57 -0400

    communicating from child to parent via deeply nested callbacks

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -5,7 +5,8 @@ import VideoList from './VideoList';
 
 class App extends React.Component {
     state = {
-        videos: []
+        videos: [],
+        selectedVideo: null
     }
 
     onTermSubmit = async term => {
@@ -16,16 +17,21 @@ class App extends React.Component {
         });
 
         this.setState({ videos: response.data.items });
-    }
+    };
+
+    onVideoSelect = (video) => {
+        console.log('From the App!', video);
+    };
+
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
     }
-}
+};
 
 export default App;
