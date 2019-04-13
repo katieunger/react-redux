773ab535d8569b7a99a9e57257f40bab90205265
Katie Unger <katie.e.unger@gmail.com>
2019-04-13 00:02:48 -0400

    wiring up the provider

--- songs/src/reducers/index.js
+++ songs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 const songsReducer = () => {
     return [
         { title: 'Space Oddity', duration: '5:18'},
@@ -14,3 +16,8 @@ const selectedSongReducer = (selectedSong=null, action) => {
 
     return selectedSong;
 };
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
