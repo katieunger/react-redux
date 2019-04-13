77d9f61e2fe88048965e81d1793d0b3fcec92633
Katie Unger <katie.e.unger@gmail.com>
2019-04-12 23:47:14 -0400

    building reducers

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,16 @@
+const songsReducer = () => {
+    return [
+        { title: 'Space Oddity', duration: '5:18'},
+        { title: 'Life On Mars?', duration: '3:51'},
+        { title: 'Heroes', duration: '3:37'},
+        { title: 'Let\'s Dance', duration: '4:10'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+};
