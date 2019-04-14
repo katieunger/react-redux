095ff4ce34063c5397ba33cdcbba82904b54620f
Katie Unger <katie.e.unger@gmail.com>
2019-04-14 10:54:33 -0400

    a little cleanup and commenting

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,5 +1,9 @@
 // Action creator
+// Note: Here we are using a named export, rather than 'export default'
+// A named export allows us to export many different functions
+// from a single file
 export const selectSong = (song) => {
+    // Return an action
     return {
         type: 'SONG_SELECTED',
         payload: song
