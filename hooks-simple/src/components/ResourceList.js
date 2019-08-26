7ceda9c37ce8ed881c8efa40efe47b7e8bbe549f
Katie Unger <katie.e.unger@gmail.com>
2019-08-26 16:50:24 -0400

    componentDidUpdate versus componentDidMount

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,10 +4,21 @@ import axios from 'axios';
 class ResourceList extends React.Component {
     state = { resources: [] };
 
+    // componentDidMount is not called a second time
     async componentDidMount() {
         const response  = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
         this.setState({ resources: response.data });
     }
+
+    // Pass previous props to test if we want to make request - so we don't make endless requests
+    // as component updates on setState()
+    async componentDidUpdate(prevProps) {
+        if (prevProps.resource !== this.props.resource) {
+            const response  = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+            this.setState({ resources: response.data });
+        }
+    }
+
 
     render() {
         return <div>{this.state.resources.length}</div>;
