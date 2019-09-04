847d33a90cd31b51da4e1993d49111f184abc937
Katie Unger <katie.e.unger@gmail.com>
2019-09-04 15:15:48 -0400

    gotcha with useEffect

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -5,14 +5,15 @@ const ResourceList = ({ resource }) => {
 
     const [resources, setResources] = useState([])
 
-    const fetchResource = async (resource) => {
-        const response  = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
-        
-        setResources(response.data);
-    }
-
     useEffect(() => {
-        fetchResource(resource);
+        // Cannot pass an async function/function which returns a promise to useEffect
+        // Must define function separately
+        // Arrow function that defines and immediately invokes a second arrow function
+        (async (resource) => {
+            const response  = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+            
+            setResources(response.data);
+        })(resource);
     }, [resource])
 
 
